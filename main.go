0892c9cbeeb7fbb68cf85f383532547a83534134
quavious <leenowon97@gmail.com>
2020-05-28 17:27:53 +0900

    nkd return

--- main.go
+++ main.go
@@ -5,23 +5,14 @@ import (
 	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I am done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	name := "lee"
-	totalLength, _ := lenAndUpper("nico")
-	fmt.Println(name)
-	fmt.Println(multiply(2, 2))
-	fmt.Println(totalLength)
-	repeatMe("nico", "lin", "super", "ill")
+	total, upper := lenAndUpper("nico")
+	fmt.Println(total, upper)
 }
