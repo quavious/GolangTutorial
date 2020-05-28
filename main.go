c78de25edd5dc4ee8cb066d2f1d958556af54062
quavious <leenowon97@gmail.com>
2020-05-28 17:19:58 +0900

    function and variables

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/quavious/learngo/something"
+	"strings"
 )
 
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
-	fmt.Println("Hello World!")
-	something.SayHello()
+	name := "lee"
+	totalLength, _ := lenAndUpper("nico")
+	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+	fmt.Println(totalLength)
+	repeatMe("nico", "lin", "super", "ill")
 }
