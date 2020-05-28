f7b6bc5e0153c98bf09ca380db3e5be0257b99d9
quavious <leenowon97@gmail.com>
2020-05-29 02:32:42 +0900

    Bank Account

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
+	"github.com/quavious/learngo/accounts"
+)
 
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico.name)
+	account := accounts.NewAccount("nico")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	account.Withdraw(20)
+	//err := account.Withdraw(20)
+	/*
+		if err != nil {
+			log.Fatalln(err)
+		}*/
+	fmt.Println(account.Balance(), account.Owner())
+	fmt.Println(account)
 }
