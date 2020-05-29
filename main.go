7eae0a43d1673a4aee2cf9cd0ae760963849977f
quavious <leenowon97@gmail.com>
2020-05-29 17:51:01 +0900

    goroutines and loop

--- main.go
+++ main.go
@@ -12,18 +12,18 @@ func _count(person string) {
 	}
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 1)
+	c <- person + " is sexy"
 }
 
 func main() {
-	c := make(chan bool)
+	c := make(chan string)
 	people := [2]string{"nico", "flynn"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 }
