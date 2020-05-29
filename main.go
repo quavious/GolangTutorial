bd91813625ce891313e2f46e5b2ce63bba7ded3a
quavious <leenowon97@gmail.com>
2020-05-29 17:28:08 +0900

    goroutines

--- main.go
+++ main.go
@@ -1,45 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request Failed")
-
-func hitURL(url string) error {
-	fmt.Println("checking : ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+func _count(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, ": in ", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
 
 func main() {
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	var results = make(map[string]string)
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go _count("nico")
+	go _count("lynn")
+	time.Sleep(time.Second * 5)
 }
