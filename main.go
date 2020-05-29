2cffec4f1e29299aca87ed33e936016a0922f108
quavious <leenowon97@gmail.com>
2020-05-29 21:27:26 +0900

    goroutine urlchecker

--- main.go
+++ main.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func _count(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, ": in ", i)
-		time.Sleep(time.Second)
+var errRequestFailed = errors.New("Request failed")
+
+func hitURL(url string, c chan<- result) {
+	fmt.Println("Checking:", url)
+	status := "OK"
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
 	}
+	c <- result{url: url, status: status}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 1)
-	c <- person + " is sexy"
+type result struct {
+	url    string
+	status string
 }
 
 func main() {
-	c := make(chan string)
-	people := [2]string{"nico", "flynn"}
-	for _, person := range people {
-		go isSexy(person, c)
+	//var results = make(map[string]string)
+	var c = make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i := 0; i < len(people); i++ {
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
 		fmt.Println(<-c)
 	}
 }
