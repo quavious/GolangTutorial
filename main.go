a85c4ef91f6263fec4f79242ae9f5432c7de2a2d
quavious <leenowon97@gmail.com>
2020-05-29 16:14:35 +0900

    url checker

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/quavious/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request Failed")
+
+func hitURL(url string) error {
+	fmt.Println("checking : ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	var results = make(map[string]string)
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
