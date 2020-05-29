ee7a2be0471b8f3361e01aa493b1ec42609830d6
quavious <leenowon97@gmail.com>
2020-05-29 15:40:08 +0900

    add

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	word := "Hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println(hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
